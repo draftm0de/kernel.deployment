776c054851df3a128c95e1831a199a3e2810e591
Fux Peter <peter.fux@autopro24.at>
2024-12-06 10:21:50 +0100

    add host:port logic

--- scripts/bash/src/dto/docker-image-name-dto.sh
+++ scripts/bash/src/dto/docker-image-name-dto.sh
@@ -4,33 +4,45 @@ set -o pipefail
 
 # split given full qualified image name into
 # example draftmode/my-image:1.0
-# - DOCKER_REGISTRY: draftmode
+# - DOCKER_REPOSITORY: draftmode
 # - DOCKER_NAME: my-image
 # - DOCKER_TAG: 1.0
 # - DOCKER_DIGEST:
 # example draftmode/my-image@sha256:1212121212
-# - DOCKER_REGISTRY: draftmode
+# - DOCKER_REPOSITORY: draftmode
 # - DOCKER_NAME: my-image
 # - DOCKER_TAG:
 # - DOCKER_DIGEST: sha256:1212121212
 
 INPUT="${1}"
+DOCKER_HOST=""
+DOCKER_PORT=""
 DOCKER_REGISTRY=""
-DOCKER_NAME="${INPUT}"
+DOCKER_REPOSITORY="${INPUT}"
 DOCKER_TAG=""
 DOCKER_DIGEST=""
-if [[ "${DOCKER_NAME}" == *"/"* ]]; then
-  DOCKER_REGISTRY="${DOCKER_NAME%%/*}"
-  DOCKER_NAME="${DOCKER_NAME#*/}"
+if [[ "${DOCKER_REPOSITORY}" == *"/"*"/"* ]]; then
+  DOCKER_HOST="${DOCKER_REPOSITORY%%/*}"
+  DOCKER_REPOSITORY="${DOCKER_REPOSITORY#*/}"
+  if [[ "${DOCKER_HOST}" == *":"* ]]; then
+    DOCKER_PORT="${DOCKER_HOST#*:}"
+    DOCKER_HOST="${DOCKER_HOST%%:*}"
+  fi
 fi
-if [[ "${DOCKER_NAME}" == *"@"* ]]; then
-  DOCKER_DIGEST="${DOCKER_NAME#*@}"
-  DOCKER_NAME="${DOCKER_NAME%%@*}"
-elif [[ "${DOCKER_NAME}" == *":"* ]]; then
-  DOCKER_TAG="${DOCKER_NAME#*:}"
-  DOCKER_NAME="${DOCKER_NAME%%:*}"
+if [[ "${DOCKER_REPOSITORY}" == *"/"* ]]; then
+  DOCKER_REGISTRY="${DOCKER_REPOSITORY%%/*}"
+  DOCKER_REPOSITORY="${DOCKER_REPOSITORY#*/}"
 fi
+if [[ "${DOCKER_REPOSITORY}" == *"@"* ]]; then
+  DOCKER_DIGEST="${DOCKER_REPOSITORY#*@}"
+  DOCKER_REPOSITORY="${DOCKER_REPOSITORY%%@*}"
+elif [[ "${DOCKER_REPOSITORY}" == *":"* ]]; then
+  DOCKER_TAG="${DOCKER_REPOSITORY#*:}"
+  DOCKER_REPOSITORY="${DOCKER_REPOSITORY%%:*}"
+fi
+export DOCKER_HOST
+export DOCKER_PORT
 export DOCKER_REGISTRY
-export DOCKER_NAME
+export DOCKER_REPOSITORY
 export DOCKER_TAG
 export DOCKER_DIGEST
