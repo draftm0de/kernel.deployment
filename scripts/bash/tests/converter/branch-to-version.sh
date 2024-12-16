346d73683be35a7cd2d615485097352ac1d965f8
Peter Fux <peter.fux@draftmode.io>
2024-12-16 23:41:07 +0100

    reset

---
+++ scripts/bash/tests/converter/branch-to-version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+base_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+base_path="$base_path/../../src"
+SCRIPT="${base_path}/converter/branch-to-version.sh"
+
+shTest "${SCRIPT}" "1" "1"
+shTest "${SCRIPT}" "{true}" "1" "--silent"
+shTest "${SCRIPT}" "{false}" "1prod" "--silent"
+shTest "${SCRIPT}" "v1" "v1"
+shTest "${SCRIPT}" "v1.2" "v1.2"
+shTest "${SCRIPT}" "v1.2.3" "v1.2.3"
+shTest "${SCRIPT}" "v1.2.3-prod" "v1.2.3-prod"
+shTest "${SCRIPT}" "" "DF20241210"
+shTest "${SCRIPT}" "v1.2.3" "v1.2.3" "--contains=v1.2"
+shTest "${SCRIPT}" "" "v1.2.3" "--contains=v1.3"
+shTest "${SCRIPT}" "v1.2.*" "v1.2.3" "--format=tag-list"
+shTest "${SCRIPT}" "v1.2.*-prod" "v1.2.3-prod" "--format=tag-list"
+shTest "${SCRIPT}" "v1.*-prod" "v1.2-prod" "--format=tag-list"
